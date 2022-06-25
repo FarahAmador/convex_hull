5f198bb39194105e4d7101e839271ff911b273b7
FarahAmador <farahamador@hotmail.com>
2022-06-25 19:53:51 +0200

    prints the convex hull of a triangle

--- convex_hull.py
+++ convex_hull.py
@@ -1,6 +1,4 @@
-    
 import numpy as np
-
 
 def slope_(p:tuple, q:tuple, r:tuple):
     slope_1 = (q[1] - p[1]) / (q[0]- p[0])
@@ -9,25 +7,37 @@ def slope_(p:tuple, q:tuple, r:tuple):
     a = slope_1 - slope_2
     b = slope_2 - slope_3
     return a,b
- 
 
 def collinear(a,b):
     if a==b:
-        print("Son puntos colineales")
-    return "Error"
+        print("ATTENTION: collineals points")
+    else:
+        print("The points form a triangle")
+    return 
 
-def collinear_area(p, q, r):
+def area(p, q, r):
     d1 = ((q[1]-p[1])**2 + (q[0]-p[0])**2)**(1.0/2.0)
     d2 = ((r[1]-q[1])**2 + (r[0]-q[0])**2)**(1.0/2.0)
     d3 = ((r[1]-p[1])**2 + (r[0]-p[0])**2)**(1.0/2.0)
     ss = (d1 + d2 + d3) / 2
-    area = (ss *(ss - d1)*(ss - d2)*(ss - d3))** (1.0 / 2.0)
-    return area
-    
+    area_triangle = (ss *(ss - d1)*(ss - d2)*(ss - d3))** (1.0 / 2.0)
+    return area_triangle
+
+def convex_hull_triangle(area_triangle, p, q, r):
+    if area_triangle == 0:
+        print("That point don't have the requirements to form a convex hull")
+    else:
+        print("The convex hull is given for"%p,q,r,p)
+    return
+
+
 p = np.array([0,10])
 q = np.array ([5, 10])
 r = np.array([20,10])
 
-x = slope_(p,q,r)
+a, b = slope_(p,q,r)
+area_triangle = area(p,q,r)
 
-print (collinear)
+print (collinear(a, b))
+
+print (convex_hull_triangle(area_triangle,p,q,r))
