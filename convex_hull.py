36c1cb4491965d9b036f362d67dde40de4b0b435
Farah Amador <farahamador@hotmail.com>
2022-06-25 10:36:10 -0600

    Changes of code

--- convex_hull.py
+++ convex_hull.py
@@ -1,15 +1,33 @@
+    
+import numpy as np
 
 
-
-def slope(p[0], p[1], q[0], q[1], r[0], r[1]):
+def slope_(p:tuple, q:tuple, r:tuple):
     slope_1 = (q[1] - p[1]) / (q[0]- p[0])
     slope_2 = (r[1] - q[1]) / (r[0]- q[0])
-    slope_3  = (r[1] - p[1]) / (r[0]- p[0])
-    return slope_1, slope_2, slope_3
+    slope_3 = (r[1] - p[1]) / (r[0]- p[0])
+    a = slope_1 - slope_2
+    b = slope_2 - slope_3
+    return a,b
+ 
 
-p = [0,2]
-q = (5, 2)
-s = (7, 2)
+def collinear(a,b):
+    if a==b:
+        print("Son puntos colineales")
+    return "Error"
 
-print(slope_1)
-print(slope_2)
+def collinear_area(p, q, r):
+    d1 = ((q[1]-p[1])**2 + (q[0]-p[0])**2)**(1.0/2.0)
+    d2 = ((r[1]-q[1])**2 + (r[0]-q[0])**2)**(1.0/2.0)
+    d3 = ((r[1]-p[1])**2 + (r[0]-p[0])**2)**(1.0/2.0)
+    ss = (d1 + d2 + d3) / 2
+    area = (ss *(ss - d1)*(ss - d2)*(ss - d3))** (1.0 / 2.0)
+    return area
+    
+p = np.array([0,10])
+q = np.array ([5, 10])
+r = np.array([20,10])
+
+x = slope_(p,q,r)
+
+print (collinear)
